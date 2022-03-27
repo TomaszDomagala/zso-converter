3c1dff05e5902234627fd8499fa33a9287911c81
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2022-03-27 02:22:29 +0000

    Wip

--- Makefile
+++ Makefile
@@ -2,16 +2,11 @@ all: converter
 
 ROOT_DIR = $(shell pwd)
 
-
-copy-qemu: ## Copy project to qemu machine
-	@echo "Copying project to qemu machine"
-	scp -P 2222 -r $(ROOT_DIR)/example root@localhost:~/
-
 converter: src/main.c
 	@echo "Building..."
-	gcc -Wall -Werror -O2 -o $@ $<
-	cp $(ROOT_DIR)/$@ $(ROOT_DIR)/example
-	cp $(ROOT_DIR)/$@ $(ROOT_DIR)/z1_test
+	@gcc -Wall -Werror -O2 -o $@ $<
+	@cp $(ROOT_DIR)/$@ $(ROOT_DIR)/example
+	@cp $(ROOT_DIR)/$@ $(ROOT_DIR)/z1_test
 
 
 .PHONY: help
--- src/main.c
+++ src/main.c
@@ -1,5 +1,25 @@
 #include "stdio.h"
 #include "elf.h"
+#include "string.h"
+
+// typedef struct
+// {
+//   unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
+//   Elf64_Half	e_type;			/* Object file type */
+//   Elf64_Half	e_machine;		/* Architecture */
+//   Elf64_Word	e_version;		/* Object file version */
+//   Elf64_Addr	e_entry;		/* Entry point virtual address */
+//   Elf64_Off	e_phoff;		/* Program header table file offset */
+//   Elf64_Off	e_shoff;		/* Section header table file offset */
+//   Elf64_Word	e_flags;		/* Processor-specific flags */
+//   Elf64_Half	e_ehsize;		/* ELF header size in bytes */
+//   Elf64_Half	e_phentsize;		/* Program header table entry size */
+//   Elf64_Half	e_phnum;		/* Program header table entry count */
+//   Elf64_Half	e_shentsize;		/* Section header table entry size */
+//   Elf64_Half	e_shnum;		/* Section header table entry count */
+//   Elf64_Half	e_shstrndx;		/* Section header string table index */
+// } Elf64_Ehdr;
+
 
 // Usage: ./converter <ET_REL file> <functions file> <output ET_REL file>
 int main(int argc, char *argv[]) {
@@ -12,6 +32,24 @@ int main(int argc, char *argv[]) {
         printf("Could not open %s\n", argv[1]);
         return 1;
     }
+
+    Elf64_Ehdr ehdr;
+    fread(&ehdr, sizeof(ehdr), 1, in);
+    fclose(in);
+
+    printf("e_type: %d\n", ehdr.e_type);
+    printf("e_machine: %d\n", ehdr.e_machine);
+    printf("e_version: %d\n", ehdr.e_version);
+    printf("e_entry: %ld\n", ehdr.e_entry);
+    printf("e_phoff: %ld\n", ehdr.e_phoff);
+    printf("e_shoff: %ld\n", ehdr.e_shoff);
+    printf("e_flags: %d\n", ehdr.e_flags);
+    printf("e_ehsize: %d\n", ehdr.e_ehsize);
+    printf("e_phentsize: %d\n", ehdr.e_phentsize);
+    printf("e_phnum: %d\n", ehdr.e_phnum);
+    printf("e_shentsize: %d\n", ehdr.e_shentsize);
+    printf("e_shnum: %d\n", ehdr.e_shnum);
+    printf("e_shstrndx: %d\n", ehdr.e_shstrndx);
 
     return 0;
 }
